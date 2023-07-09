eab9d275d468ab8bd29e8a5cb75766ee584e8b6e
Caleb Sullivan <CalebSullivan357@gmail.com>
2023-07-09 00:44:43 -0500

    whip

--- word-game/src/components/Game.tsx
+++ word-game/src/components/Game.tsx
@@ -2,8 +2,84 @@
 
 export function Game() {
   return (
-    <div className="bg-white p-4 rounded-lg">
-      <p>Hello From Game</p>
+    <div className="rounded-lg bg-white p-4 max-w-xl space-y-6">
+      <Question />
+      <GivenWordOptions />
+      <CurrentSentenceBuilt />
     </div>
   );
+}
+
+// question component
+
+function Question() {
+  return (
+    <div>
+      <p className="mb-4 text-center">Why does marissa think I snore at night?</p>
+    </div>
+  );
+}
+
+const wordOptions = [
+  "Always",
+  "Appreciate",
+  "Beautiful",
+  "Calm",
+  "Care",
+  "Celebrate",
+  "Charm",
+  "Clean",
+  "Comfort",
+  "Compassion",
+  "Connect",
+  "Contentment",
+  "Courage",
+  "Create",
+  "Delight",
+  "Dream",
+  "Elegance",
+  "Embrace",
+  "Encourage",
+  "Enjoy",
+  "Excite",
+  "Explore",
+  "Faith",
+  "Family",
+  "Flourish",
+  "Focus",
+  "Friendship",
+  "Generosity",
+  "Gentle",
+  "Genuine",
+  "Grace",
+  "Gratitude",
+  "Happiness",
+  "Harmony",
+  "Health",
+  "Heartfelt",
+  "Honest",
+  "Hope",
+  "Imagine",
+  "Inspire",
+  "Joy",
+];
+
+function GivenWordOptions() {
+  const wordButtons = wordOptions.map((wordOption) => {
+    return <button className="bg-gray-100 rounded-lg px-4 py-2 hover:bg-gray-200">{wordOption}</button>;
+  });
+
+  return (
+    <div className="gap-4 flex flex-wrap">
+      {wordButtons}
+    </div>
+  );
+}
+
+function CurrentSentenceBuilt(){
+    return(
+        <div className="">
+            <p>This is a template Sentence</p>
+        </div>
+    )
 }
